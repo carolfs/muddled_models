f4cf13d16631c85295cc96859493dac607a6ba45
Carolina Feher da Silva <carolfsu@gmail.com>
2019-07-01 18:14:30 +0200

    Function to download data from Kool et al. (2016)

--- utils.py
+++ utils.py
@@ -1,11 +1,30 @@
+# Copyright (C) 2019  Carolina Feher da Silva
+
+# This program is free software: you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation, either version 3 of the License, or
+# (at your option) any later version.
+
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+
+# You should have received a copy of the GNU General Public License
+# along with this program.  If not, see <https://www.gnu.org/licenses/>.
+
 """Common Python functions used in many of the analyses."""
 
 import os
 import pickle
 import random
+import io
 import pystan
 import numpy as np
+import pandas as pd
 from numba import jit, vectorize, float64
+import requests
+from scipy.io import loadmat
 
 def get_stan_model(textfn, binfn):
     """Loads a Stan model, compiling it if necessary."""
@@ -53,3 +72,74 @@ def get_random_fstate(choice):
     if random.random() < COMMON_PROB:
         return choice
     return 1 - choice
+
+# Link to the data from Kool et al. (2016) we analyzed in this paper
+KOOL_2016_DATA = 'https://github.com/wkool/tradeoffs/blob/master/data/daw%20paradigm/data.mat?raw=true'
+KOOL_2016_VARS = {
+    'stim_1_left': 1,
+    'stim_1_right': 2,
+    'rt_1': 3,
+    'choice1': 4,
+    'stim_2_left': 5,
+    'stim_2_right': 6,
+    'rt_2': 7,
+    'choice2': 8,
+    'win': 9,
+    'state2': 10,
+    'common': 11,
+    'score': 12,
+    'ps1a1': 14,
+    'ps1a2': 15,
+    'ps2a1': 16,
+    'ps2a2': 17,
+    'trial': 18,
+}
+KOOL_2016_CSV_FILE = 'results/kool_2016_data.csv'
+
+def load_kool_2016_data():
+    "Loads the data from Kool et al. (2016) as a pandas DataFrame."
+    if not os.path.exists(KOOL_2016_CSV_FILE):
+        fetch_kool_2016_data()
+    dtype = {
+        'participant': int,
+        'stim_1_left': int,
+        'stim_1_right': int,
+        'rt_1': int,
+        'choice1': int,
+        'stim_2_left': int,
+        'stim_2_right': int,
+        'rt_2': int,
+        'choice2': int,
+        'win': int,
+        'state2': int,
+        'common': int,
+        'score': int,
+        'trial': int,
+    }
+    data = pd.read_csv(KOOL_2016_CSV_FILE, dtype=dtype)
+    return data
+
+def fetch_kool_2016_data():
+    "Fetches the data from Kool et al. (2016) off its Github repository."
+    data = requests.get(KOOL_2016_DATA)
+    data = loadmat(io.BytesIO(data.content))['data']
+    name_vars = list(KOOL_2016_VARS.keys())
+    name_vars.sort()
+    try:
+        with open(KOOL_2016_CSV_FILE, 'w') as outf:
+            outf.write('participant,{}\n'.format(','.join(name_vars)))
+            for trial in data:
+                # Exclude practice trials
+                if int(trial[13]) == 1:
+                    continue
+                assert str(trial[0][0]).startswith('subject_')
+                participant = int(str(trial[0][0]).split('_')[1]) - 1
+                assert participant < 206
+                values = []
+                for var in name_vars:
+                    value = trial[KOOL_2016_VARS[var]][0][0]
+                    values.append(str(value))
+                outf.write('{},{}\n'.format(participant, ','.join(values)))
+    except:
+        os.remove(KOOL_2016_CSV_FILE)
+        raise
